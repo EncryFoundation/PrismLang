5223ab3f419c24585d006d2a701bd4f3220f2310
george <george@MacBook-Pro-george.local>
2018-07-10 18:04:53 +0300

    added cases to be fixed

--- src/test/scala/org/encryfoundation/prismlang/integration/SyntaxSpec.scala
+++ src/test/scala/org/encryfoundation/prismlang/integration/SyntaxSpec.scala
@@ -6,23 +6,26 @@ import org.scalatest.{Matchers, PropSpec}
 
 class SyntaxSpec extends PropSpec with Matchers with Parser with TestCompiler {
 
-  property(testName = "SSA"){
-    val correctOrder = """
+  property(testName = "SSA") {
+    val correctOrder =
+      """
                 {let a = 10
-               | let b = 10
-               | let c = a + b}
+                |let b = 10
+                |let c = a + b}
       """.stripMargin
 
-    val doubleAssignment = """
+    val doubleAssignment =
+      """
                 {let a = 10
-                | let a = 15}
+                |let a = 15}
       """.stripMargin
 
-    val variableBeforeAssignment ="""
+    val variableBeforeAssignment =
+      """
                {let c = a + 10
-                | let a = 25}
+                |let a = 25}
       """.stripMargin
-    
+
     val functionBeforeAssignment =
       """{let c = sum(4,5)
         |def sum(a: Int, b: Int): Int = {
@@ -37,86 +40,11 @@ class SyntaxSpec extends PropSpec with Matchers with Parser with TestCompiler {
         |let c : Int = 5
         |let d : Int = 7
         |let g = sum(c,d)}""".stripMargin
-
-    val successMusk = Seq(true, false, false, false, true)
-    val tries = Seq(correctOrder, doubleAssignment, variableBeforeAssignment,
-      functionBeforeAssignment, functionCorrectOrder)
-      .map(e =>compileExpr(parse(e).get.head))
-      .zip(successMusk).
-      map(e => e._1.isSuccess shouldBe e._2)
-  }
-
-  property (testName = "Infinite Loop"){
-
-  }
-
-  property (testName = "stack overflow"){
-    val hugeArrayRepresent = getArrayString(List.range(1,100000))
-    val light =
-      s"""{let a : Array[Int] = $hugeArrayRepresent}""".stripMargin
-    val successMusk = Seq(false)
-    val tries = Seq(light)
-      .map(e => compileExpr(parse(e).get.head))
-      .zip(successMusk).
-      map(e => e._1.isSuccess shouldBe e._2)
-  }
-
-  property(testName = "Type resolving"){
-
-    val query0 = "{let a : Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)}"
-
-    val query1 = """
-        {let b = 20
-        |let c = true
-        |let d = b + c}
-      """.stripMargin
-
-    val byte_num = 192.toByte
-
-    val query2 = s"""
-        {let v : Byte = $byte_num
-         | let a : Int = 77
-         | let w = a/v
-        }
-      """.stripMargin
-
-// FIXME in later version add Error pop up for user
-//    val query3 =
-//      """{let G : Array[Int] = Array(1,2,5,7.6)}
-//      """.stripMargin
-
-    val query4 =
-      """
-        {let V : Array[Int] = Array(1,2,3)
-        |let W : Array[Int] = Array(3,4,5)
-        |let Z = V + W}
-      """.stripMargin
-
-    val successMusk = Seq(true, false, false, false, false)
-    val tries = Seq(query0, query1, query2, query4)
-      .map(e => compileExpr(parse(e).get.head))
-      .zip(successMusk).
-      map(e => e._1.isSuccess shouldBe e._2)
-  }
-
-  property(testName = "syntax constructions"){
-
-    val toByteCast = "let b : Byte = (144).toByte"
-
-    val longMax = Long.MaxValue
-    val letLongMaxNumber =
-      s"""{let a : Int = $longMax}""".stripMargin
-
-    val conditionalVariableDeclaration =
-      """{let f : Bool = if (7==7){
-          |true} else {
-          |false}}""".stripMargin
-
-    val wrongConditionalVariable = "{let g : Int = 47 if (5<2) else 22}"
-
-    val ifElseStatementTypeResolving  =
+//FIXME variable reassignment works inside if-else statement
+    val ifElseStatementReassignment =
       """{let a : Int = 7
-        |let c : Byte = (177).toByte
+        |let b : Int = 8
+        |let c : Byte = (120).toByte
         |if (c > a){
         |  let b = c
         |} else {
@@ -124,15 +52,143 @@ class SyntaxSpec extends PropSpec with Matchers with Parser with TestCompiler {
         |   }
         |}""".stripMargin
 
-    val successMusk = Seq(true, false, true, false, false)
-
-    val tries = Seq(toByteCast, letLongMaxNumber, conditionalVariableDeclaration,
-      wrongConditionalVariable, ifElseStatementTypeResolving)
-      .map(e => compileExpr(parse(e).get.head))
-      .zip(successMusk).
-      map(e => e._1.isSuccess shouldBe e._2)
+    compiled(correctOrder).isSuccess shouldBe true
+    compiled(doubleAssignment).isSuccess shouldBe false
+    compiled(variableBeforeAssignment).isSuccess shouldBe false
+    compiled(functionBeforeAssignment).isSuccess shouldBe false
+    compiled(functionCorrectOrder).isSuccess shouldBe true
+    compiled(ifElseStatementReassignment).isSuccess shouldBe false
   }
 
-  def getArrayString(sample : List[Any]) : String = sample.mkString("Array(", ", ", ")")
+  property(testName = "Infinite Loop") {
+
+  }
+
+  property(testName = "stack overflow") {
+    val constraintArray = getArrayString(List.range(1,300))
+    val bigArray = getArrayString(List.range(1, 100000))
+
+    val declareConstraintArray =
+      s"""{let a : Array[Int] = $constraintArray}""".stripMargin
+
+    val declareBigArray =
+      s"""{let a : Array[Int] = $bigArray}""".stripMargin
+
+    compiled(declareConstraintArray).isSuccess shouldBe true
+    compiled(declareBigArray).isSuccess shouldBe false
+
+    val hugeArray = getArrayString(List.range(1, 1000000))
+
+    val declareHugeArray =
+      s"""{let a : Array[Int] = $hugeArray}""".stripMargin
+
+    compiled(declareHugeArray).isSuccess shouldBe false
+  }
+
+  property(testName = "Type resolving") {
+
+    val arrayDeclaration = "{let a : Array[Int] = Array(1, 2, 3, 4, 5, 6, 7, 8, 9)}"
+
+    val sumBoolAndInt =
+      """
+        {let b = 20
+        |let c = true
+        |let d = b + c}
+      """.stripMargin
+
+    val byte_num = 101.toByte
+
+    val divideIntOnByte =
+      s"""
+        {let v : Byte = $byte_num
+         |let a : Int = 77
+         |let w = a/v
+        }
+      """.stripMargin
+
+    val intDivisionRight = """
+        {let b = 20
+        |let c = 5
+        |let d = b/c}
+      """.stripMargin
+
+    val intDivisionWrong = """
+        {let b = 20
+        |let c = 3
+        |let d = b/c}
+      """.stripMargin
+
+    // FIXME in later version add Error pop up for user (for usage of float variables)
+    val mixedTypesInArray =
+      """{let G : Array[Int] = Array(1,2,5,true)}
+      """.stripMargin
+
+    val arrayAddInt = """
+        {let V : Array[Int] = Array(1,2,3)
+        |let w : Int = 7
+        |let Z = V + w}
+      """.stripMargin
+
+    val arrayConcatenation =
+      """
+        {let V : Array[Int] = Array(1,2,3)
+        |let W : Array[Int] = Array(3,4,5)
+        |let Z = V + W}
+      """.stripMargin
+
+    compiled(arrayDeclaration).isSuccess shouldBe true
+    compiled(sumBoolAndInt).isSuccess shouldBe false
+    compiled(divideIntOnByte).isSuccess shouldBe false
+    compiled(intDivisionRight).isSuccess shouldBe true
+    compiled(intDivisionWrong).isSuccess shouldBe false
+    compiled(mixedTypesInArray).isSuccess shouldBe false
+    compiled(arrayAddInt).isSuccess shouldBe false
+    compiled(arrayConcatenation).isSuccess shouldBe false
+  }
+
+  property(testName = "syntax constructions") {
+
+    val toByteCast = "let b : Byte = (101).toByte"
+    val longMin = Long.MinValue
+    val longMax = Long.MaxValue
+//FIXME Long.MinValue assignment throws exception
+    val letLongMinNumber =
+      s"""{let a : Int = $longMin}""".stripMargin
+
+    val letLongMaxNumber =
+      s"""{let a : Int = $longMax}""".stripMargin
+
+    val conditionalVariableDeclaration =
+      """{let f : Bool = if (7==7)
+        |{
+        | true
+        | } else {
+        |false
+        | }
+        |}""".stripMargin
+//FIXME Python ternary operator works like let g : Int = 47
+    val wrongConditionalVariable = "{let g : Int = 47 if (5<2) else 22}"
+
+    val ifElseStatementTypeResolving =
+      """{let a : Int = 7
+        |let c : Byte = (120).toByte
+        |if (c > a){
+        |  let b = c
+        |} else {
+        |   let b = a
+        |   }
+        |}""".stripMargin
+
+    compiled(toByteCast).isSuccess shouldBe true
+    compiled(letLongMinNumber).isSuccess shouldBe true
+    compiled(letLongMaxNumber).isSuccess shouldBe true
+    compiled(conditionalVariableDeclaration).isSuccess shouldBe true
+    compiled(wrongConditionalVariable).isSuccess shouldBe false
+    compiled(ifElseStatementTypeResolving).isSuccess shouldBe true
+  }
+
+  def getArrayString(sample: List[Any]): String = sample.mkString("Array(", ", ", ")")
+
+  def compiled(prismScript: String) = compileExpr(parse(prismScript).get.head)
 
 }
