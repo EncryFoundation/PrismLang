dfd92b799e3bbc19c9a07ce232858b5eb8526094
Ilya Oskin <ilya.arcadich@gmail.com>
2018-07-26 14:54:16 +0300

    AST overwriting rules for `ifLet` node improved. (#30)
    * AST overwriting rules for `ifLet` node improved.
    
    * Multiline boolexp chains support.
    
    * `Filter()` support added to costEstimator.
    
    * Alignment
    
    * Alignment (2)

--- src/main/scala/org/encryfoundation/prismlang/core/CostTable.scala
+++ src/main/scala/org/encryfoundation/prismlang/core/CostTable.scala
@@ -2,34 +2,35 @@ package org.encryfoundation.prismlang.core
 
 object CostTable {
 
-  val ConstDeclarationC: Int = 10
-  val FuncDeclarationC: Int  = 20
-  val LambDeclarationC: Int  = 5
-  val BlockDeclarationC: Int = 5
+  val ConstDeclarationC: Int     = 10
+  val FuncDeclarationC: Int      = 20
+  val LambDeclarationC: Int      = 5
+  val BlockDeclarationC: Int     = 5
   val NestedBlockPenalty: Double = 1.2
 
-  val NameRefC: Int          = 2
-  val CallC: Int             = 4
-  val BoolOpC: Int           = 5
-  val BinOpC: Int            = 8
-  val CompOpC: Int           = 4
-  val IfC: Int               = 5
-  val IfLetC: Int            = 10
-  val UnaryOpC: Int          = 1
-  val SubscriptC: Int        = 8
+  val NameRefC: Int              = 2
+  val CallC: Int                 = 4
+  val BoolOpC: Int               = 5
+  val BinOpC: Int                = 8
+  val CompOpC: Int               = 4
+  val IfC: Int                   = 5
+  val IfLetC: Int                = 10
+  val UnaryOpC: Int              = 1
+  val SubscriptC: Int            = 8
 
   // Constants
-  val IntConstC: Int         = 1
-  val ByteConstC: Int        = 1
-  val CharC: Int             = 2
-  val CollEltC: Int          = 1
-  val TupleEltC: Int         = 1
-  val BoolLeafC: Int         = 1
+  val IntConstC: Int             = 1
+  val ByteConstC: Int            = 1
+  val CharC: Int                 = 2
+  val CollEltC: Int              = 1
+  val TupleEltC: Int             = 1
+  val BoolLeafC: Int             = 1
 
-  val DecodingC: Int         = 4
+  val DecodingC: Int             = 4
 
-  val MapC: Int              = 4
-  val SizeOfC: Int           = 2
-  val SumC: Int              = 2
-  val ExistsC: Int           = MapC
+  val MapC: Int                  = 4
+  val FiltC: Int                 = 4
+  val SizeOfC: Int               = 2
+  val SumC: Int                  = 2
+  val ExistsC: Int               = MapC
 }
--- src/test/scala/org/encryfoundation/prismlang/compiler/PCompilerSpec.scala
+++ src/test/scala/org/encryfoundation/prismlang/compiler/PCompilerSpec.scala
@@ -69,4 +69,58 @@ class PCompilerSpec extends PropSpec with Matchers {
 
     compiledTry.get.toString shouldEqual expected.toString
   }
+
+  property("Large module compilation") {
+    val source: String =
+      """
+        |contract (signature: Signature25519, transaction: Transaction, self: AssetBox) = {
+        |  let rate = 2
+        |  let lessBase = true
+        |  let acceptableTokenId = Array((101).toByte, (100).toByte)
+        |  let yourRegularContractHash = base58'GtBn7qJwK1v1EbB6CZdgmkcvt849VKVfWoJBMEWsvTew'
+        |
+        |  if (let flag : AssetBox = transaction) {
+        |    flag.contractHash == yourRegularContractHash
+        |  } else false
+        |
+        |  def getNumToExchange(amount: Int, rate: Int, lessBase: Bool): Int = {
+        |    if(lessBase) {
+        |      if (amount%rate==0) amount/rate else amount/rate + 1
+        |    } else {
+        |      amount * rate
+        |    }
+        |  }
+        |
+        |  def amAbleToOpen(box: Box, yourRegularContractHash: Array[Byte]): Bool = {
+        |    if (let assetBox : AssetBox = box) {
+        |      assetBox.contractHash == yourRegularContractHash
+        |    } else false
+        |  }
+        |
+        |  def getAmountFromBox(tokenId: Array[Byte], box : Box): Int = {
+        |    if (let flag1: AssetBox = box) {
+        |      if (flag1.tokenId == tokenId) flag1.amount else 0
+        |    } else 0
+        |  }
+        |
+        |  def hasTokenId(tokenId: Array[Byte], box : Box): Bool = {
+        |    if(let assetBox : AssetBox = box) {
+        |      if(assetBox.tokenId == tokenId) true else false
+        |    } else false
+        |  }
+        |
+        |  let buyAmount = getAmountFromBox(transaction.outputs.filter(lamb(x: Box) = !hasTokenId(acceptableTokenId, x))[0])
+        |  let wantBuyOurs = getNumToExchange(buyAmount, rate, lessBase)
+        |  if (wantBuyOurs > 0){
+        |    let changeBox = transaction.outputs.filter(lamb(x: Box) = !hasTokenId(self.tokenId, x))[0]
+        |    let declaredChangeAmount = getAmountFromBox(changeBox)
+        |    if ((self.amount - wantBuyOurs == declaredChangeAmount) && amAbleToOpen(changeBox, yourRegularContractHash)) true else false
+        |  } else false
+        |}
+      """.stripMargin
+
+    val compiledTry: Try[CompiledContract] = PCompiler.compile(source)
+
+    compiledTry.isSuccess shouldBe true
+  }
 }
