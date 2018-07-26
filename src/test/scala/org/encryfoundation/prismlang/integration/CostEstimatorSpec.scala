a87844de81d709d2816f587cbadb1cbfba80cf89
Ilya Oskin <ilya.arcadich@gmail.com>
2018-07-26 13:11:39 +0300

    Empty contracts removed.

--- src/test/scala/org/encryfoundation/prismlang/integration/CostEstimatorSpec.scala
+++ src/test/scala/org/encryfoundation/prismlang/integration/CostEstimatorSpec.scala
@@ -1,10 +1,10 @@
 package org.encryfoundation.prismlang.integration
 
-import org.encryfoundation.prismlang.compiler.{CompiledContract, PCompiler}
 import org.scalatest.PropSpec
 import org.scalatest.concurrent.TimeLimits
 import org.scalatest.time.SpanSugar._
-import scala.util.{Failure, Success, Try}
+
+import scala.util.Try
 
 class CostEstimatorSpec extends PropSpec with Utils with TimeLimits {
   property("Nested block attack") {
@@ -26,63 +26,6 @@ class CostEstimatorSpec extends PropSpec with Utils with TimeLimits {
     failAfter(60 seconds) {
       val tryCost: Try[Int] = estimateCost(compiled(source).get)
       tryCost.isSuccess shouldBe false
-    }
-  }
-
-  property("Complex Contract Check") {
-    val contractString =
-      """
-        |contract (signature: Signature25519, transaction: Transaction) = {
-        |  let rate = 2
-        |  let lessBase = true
-        |  let acceptableTokenId = Array((101).toByte, (100).toByte)
-        |  let yourRegularContractHash = base58'GtBn7qJwK1v1EbB6CZdgmkcvt849VKVfWoJBMEWsvTew'
-        |
-        |  def getNumToExchange(amount: Int, rate: Int, lessBase: Bool): Int = {
-        |    if(lessBase) {
-        |      if (amount%rate==0) amount/rate else amount/rate + 1
-        |    } else {
-        |      amount * rate
-        |    }
-        |  }
-        |
-        |  def amAbleToOpen(box: Box, yourRegularContractHash: Array[Byte]): Bool = {
-        |    if(let assetBox: AssetBox = box) {
-        |      assetBox.contractHash == yourRegularContractHash
-        |    } else false
-        |  }
-        |
-        |  def getAmountFromBox(tokenId: Array[Byte], box : Box): Int = {
-        |    if(let assetBox: AssetBox = box) {
-        |      if(assetBox.tokenId == tokenId) assetBox.amount else 0
-        |    } else 0
-        |  }
-        |
-        |  def hasTokenId(tokenId: Array[Byte], box : Box): Bool = {
-        |    if(let assetBox: AssetBox = box) {
-        |      if(assetBox.tokenId == tokenId) true else false
-        |    } else false
-        |  }
-        |
-        |  let byuAmount = getAmountFromBox(transaction.outputs.filter(lamb(x: Box) = !hasTokenId(acceptableTokenId, x))[0])
-        |  let wantBuyOurs = getNumToExchange(buyAmount, rate, lessBase)
-        |  if (wantBuyOurs > 0){
-        |    let changeBox = transaction.outputs.filter(lamb(x: Box) = !hasTokenId(self.tokenId, x))[0]
-        |    let declaredChangeAmount = getAmountFromBox(changeBox)
-        |    if ((self.amount - wantBuyOurs == declaredChangeAmount) && amAbleToOpen(changeBox, yourRegularContractHash)) true else false
-        |  } else false
-        |}
-      """.stripMargin
-
-    val tryContract: Try[CompiledContract] = PCompiler.compile(contractString)
-    tryContract.get
-    tryContract.isSuccess shouldBe true
-    tryContract match {
-      case Success(contract) => estimateCost(contract.script) match {
-        case Success(cost) => cost shouldEqual 975
-        case Failure(e) => throw e
-      }
-      case _ => fail
     }
   }
 }
