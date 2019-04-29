6f7422eeb01850c851706e13ce4763fe5854cc3d
aleksandr <alexbromel@gmail.com>
2019-04-29 15:49:48 +0300

    add toString to in

--- src/main/scala/org/encryfoundation/prismlang/core/Ast.scala
+++ src/main/scala/org/encryfoundation/prismlang/core/Ast.scala
@@ -243,6 +243,7 @@ object Ast {
     }
 
     case object In extends CompOp {
+      override def toString: String = "in"
       lazy val leftTypeResolution: List[Types.PType] = Types.regularTypes
       lazy val rightTypeResolution: List[Types.PType] = Types.regularTypes.map(PCollection.apply)
     }
