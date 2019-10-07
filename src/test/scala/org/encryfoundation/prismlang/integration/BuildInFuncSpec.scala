214d3ed332d4b04879c674b737e3d8fbac8761ce
capdev <sergey.moskalenko@scruge.world>
2019-10-07 15:16:02 +0500

    add blake2b256 test

--- src/test/scala/org/encryfoundation/prismlang/integration/BuildInFuncSpec.scala
+++ src/test/scala/org/encryfoundation/prismlang/integration/BuildInFuncSpec.scala
@@ -4,6 +4,9 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 import org.scalatest.PropSpec
+import scorex.crypto.encode.Base16
+import scorex.crypto.hash.{Blake2b256, Digest32}
+import scorex.utils.Random
 
 class BuildInFuncSpec extends PropSpec with Utils {
 
@@ -20,6 +23,22 @@ class BuildInFuncSpec extends PropSpec with Utils {
       """.stripMargin
 
     testCompiledExpressionWithOptionalEvaluation(sources, compilationSuccess = true, Some(true), Some(timestamp))
+  }
+
+  property("blake2b256") {
+
+    val bytes: Array[Byte] = Random.randomBytes()
+    val hash: Digest32 = Blake2b256.hash(bytes)
+
+    val sources =
+      s"""
+         |  {
+         |    let b = base16'${Base16.encode(bytes)}'
+         |    blake2b256(b)
+         |  }
+      """.stripMargin
+
+    testCompiledExpressionWithOptionalEvaluation(sources, compilationSuccess = true, Some(true), Some(hash))
   }
 
 }
