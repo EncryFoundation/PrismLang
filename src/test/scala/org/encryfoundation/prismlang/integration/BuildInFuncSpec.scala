107e347dfbf32240a3bc01583b29e153215f1a8c
capdev <sergey.moskalenko@scruge.world>
2019-10-08 14:05:36 +0500

    add Min,Max tests

--- src/test/scala/org/encryfoundation/prismlang/integration/BuildInFuncSpec.scala
+++ src/test/scala/org/encryfoundation/prismlang/integration/BuildInFuncSpec.scala
@@ -5,12 +5,14 @@ import java.util.Date
 
 import org.scalatest.PropSpec
 import scorex.crypto.encode.{Base16, Base58}
-import scorex.crypto.hash.{Blake2b256, Blake2b512, Digest32, Keccak256, Keccak512, Sha256}
-import scorex.utils.Random
+import scorex.crypto.hash.{Blake2b256, Blake2b512, Digest, Digest32, Keccak256, Keccak512, Sha256}
+import scorex.utils.{Random => ScorexRandom}
+
+import scala.util.{Random, Try}
 
 class BuildInFuncSpec extends PropSpec with Utils {
 
-  val bytes: Array[Byte] = Random.randomBytes()
+  val bytes: Array[Byte] = ScorexRandom.randomBytes()
 
   property("unixTime") {
     val sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")
@@ -27,45 +29,61 @@ class BuildInFuncSpec extends PropSpec with Utils {
     testCompiledExpressionWithOptionalEvaluation(sources, compilationSuccess = true, Some(true), Some(timestamp))
   }
 
-  property("blake2b256") {
-    checkHash(bytes, "blake2b256", Blake2b256.hash)
-  }
-
-  property("blake2b512") {
-    checkHash(bytes, "blake2b512", Blake2b512.hash)
-  }
-
-  property("keccak256") {
-    checkHash(bytes, "keccak256", Keccak256.hash)
-  }
-
-  property("keccak512") {
-    checkHash(bytes, "keccak512", Keccak512.hash)
-  }
-
-  property("sha256") {
-    checkHash(bytes, "sha256", Sha256.hash)
-  }
-
   property("base16") {
-    checkBase(bytes, "base16", Base16.encode)
+    checkBaseDecodeFunc(Base16.encode(bytes), "base16", Base16.decode)
   }
 
   property("base58") {
-    checkBase(bytes, "base58", Base58.encode)
+    checkBaseDecodeFunc(Base58.encode(bytes), "base58", Base58.decode)
   }
 
-  def checkHash(bytes: Array[Byte], hashFuncName: String, hashFunc: Array[Byte] => Array[Byte]): Unit = {
-    val hash = hashFunc(bytes)
-    val sources = s"{ $hashFuncName(base16'${Base16.encode(bytes)}') }"
+  property("blake2b256") {
+    checkHashFunc(bytes, "blake2b256", Blake2b256.hash)
+  }
+
+  property("blake2b512") {
+    checkHashFunc(bytes, "blake2b512", Blake2b512.hash)
+  }
+
+  property("keccak256") {
+    checkHashFunc(bytes, "keccak256", Keccak256.hash)
+  }
+
+  property("keccak512") {
+    checkHashFunc(bytes, "keccak512", Keccak512.hash)
+  }
+
+  property("sha256") {
+    checkHashFunc(bytes, "sha256", Sha256.hash)
+  }
+
+  property("Min") {
+    checkBinaryFunc("min", Math.min)
+  }
+
+  property("Max") {
+    checkBinaryFunc("max", Math.max)
+  }
+
+  def checkBaseDecodeFunc(str: String, funcName: String, checkFunc: String => Try[Array[Byte]]): Unit = {
+    val sources = s"{ $funcName'$str' }"
+
+    testCompiledExpressionWithOptionalEvaluation(sources, compilationSuccess = true, Some(true), Some(checkFunc(str)))
+  }
+
+  def checkHashFunc(bytes: Array[Byte], funcName: String, checkFunc: Array[Byte] => Digest): Unit = {
+    val hash = checkFunc(bytes)
+    val sources = s"{ $funcName(base16'${Base16.encode(bytes)}') }"
 
     testCompiledExpressionWithOptionalEvaluation(sources, compilationSuccess = true, Some(true), Some(hash))
   }
 
-  def checkBase(bytes: Array[Byte], funcName: String, func: Array[Byte] => String): Unit = {
-    val baseStr = func(bytes)
-    val sources = s"{ $funcName'$baseStr' }"
+  def checkBinaryFunc(funcName: String, checkFunc: (Long, Long) => Long): Unit = {
+    val a = Random.nextLong
+    val b = Random.nextLong
+    val sources = s"{ $funcName($a, $b) }"
 
-    testCompiledExpressionWithOptionalEvaluation(sources, compilationSuccess = true, Some(true), Some(bytes))
+    testCompiledExpressionWithOptionalEvaluation(sources, compilationSuccess = true, Some(true), Some(checkFunc(a, b)))
   }
+
 }
