24cf2048837aede0711464941084652d26a9c28d
capdev <sergey.moskalenko@scruge.world>
2019-10-09 18:38:48 +0500

    checkExpr refactoring

--- src/test/scala/org/encryfoundation/prismlang/evaluator/OperatorsTypeCompatibilitySpec.scala
+++ src/test/scala/org/encryfoundation/prismlang/evaluator/OperatorsTypeCompatibilitySpec.scala
@@ -4,11 +4,10 @@ import org.encryfoundation.prismlang.ValueGenerator
 import org.encryfoundation.prismlang.core.Ast.Expr._
 import org.encryfoundation.prismlang.core.Ast.{CompOp, _}
 import org.encryfoundation.prismlang.core.Types.{PBoolean, PByte, PInt}
-import org.scalatest.{Matchers, PropSpec}
+import org.encryfoundation.prismlang.integration.Utils
+import org.scalatest.PropSpec
 
-class OperatorsTypeCompatibilitySpec extends PropSpec
-  with Matchers
-  with ExprChecker {
+class OperatorsTypeCompatibilitySpec extends PropSpec with Utils {
 
   val valueTypes = List("Str", "IntConst", "ByteConst", "Base16Str", "Base58Str", "Bool")
 
@@ -22,61 +21,58 @@ class OperatorsTypeCompatibilitySpec extends PropSpec
   def boolExpr(oper: BooleanOp, values: List[Expr]) = Bool(oper, List(values(0), values(1)))
   def unaryExpr(oper: UnaryOp, values: List[Expr]) = Unary(oper, values(0))
 
-  def checkBinOperators[T](operators: List[T], values1: List[Expr], values2: List[Expr],
-                           expr: (T, List[Expr]) => Expr, expectedExceptions: List[String]) {
+  def checkBinOperators[T](operators: List[T], values1: List[Expr], values2: List[Expr], expr: (T, List[Expr]) => Expr) {
     operators.foreach { operator =>
       values1.foreach { value1 =>
         values2.foreach { value2 =>
           if(value1.tpe != value2.tpe && !exclusion(value1, value2)) {
-            checkExprForExceptions(expr(operator, List(value1, value2)), expectedExceptions)
+            checkExpr(expr(operator, List(value1, value2)))
           }
         }
       }
     }
   }
 
-  def checkUnaryOperators[T](operators: List[T], values: List[Expr],
-                             expr: (T, List[Expr]) => Expr, expectedExceptions: List[String]) {
+  def checkUnaryOperators[T](operators: List[T], values: List[Expr], expr: (T, List[Expr]) => Expr) {
     operators.foreach { operator =>
       values.foreach { value =>
-        checkExprForExceptions(expr(operator, List(value)), expectedExceptions)
+        checkExpr(expr(operator, List(value)))
       }
     }
   }
 
   property("binary operators shouldn't compile with different types") {
     checkBinOperators(List(Operator.Add, Operator.Sub, Operator.Mult, Operator.Div, Operator.Mod, Operator.Pow),
-      values1, values2, binExpr, List("SemanticAnalysisException"))
+      values1, values2, binExpr)
   }
 
   property("compare operators shouldn't compile with different types") {
-    checkBinOperators(List(CompOp.GtE, CompOp.Gt, CompOp.Lt, CompOp.LtE),
-      values1, values2, compareExpr, List("SemanticAnalysisException"))
+    checkBinOperators(List(CompOp.GtE, CompOp.Gt, CompOp.Lt, CompOp.LtE), values1, values2, compareExpr)
   }
 
   property("Eq NotEq shouldn't compile with different types") {
     checkBinOperators(List(CompOp.Eq, CompOp.NotEq),
-      values1, values2, compareExpr, List("SemanticAnalysisException"))
+      values1, values2, compareExpr)
   }
 
   property("In NotIn shouldn't compile with different types") {
-    checkBinOperators(List(CompOp.In, CompOp.NotIn), values1, values2, compareExpr, List("SemanticAnalysisException"))
+    checkBinOperators(List(CompOp.In, CompOp.NotIn), values1, values2, compareExpr)
   }
 
   property("And Or shouldn't compile with different types") {
-    checkBinOperators(List(BooleanOp.And, BooleanOp.Or), values1, values2, boolExpr, List("SemanticAnalysisException"))
+    checkBinOperators(List(BooleanOp.And, BooleanOp.Or), values1, values2, boolExpr)
   }
 
   property("Unary.Not should be compile with Boolean type only") {
     val valExprs = genValues
       .filterNot(_.tpe == PBoolean)
-    checkUnaryOperators(List(UnaryOp.Not), valExprs, unaryExpr, List("SemanticAnalysisException"))
+    checkUnaryOperators(List(UnaryOp.Not), valExprs, unaryExpr)
   }
 
   property("Unary.Invert should be compile with Int Byte types only") {
     val valExprs = genValues
       .filterNot(expr =>  List(PInt, PByte).contains(expr.tpe))
-    checkUnaryOperators(List(UnaryOp.Invert), valExprs, unaryExpr, List("SemanticAnalysisException"))
+    checkUnaryOperators(List(UnaryOp.Invert), valExprs, unaryExpr)
   }
 
 }
